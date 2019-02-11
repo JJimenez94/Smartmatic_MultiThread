eb6359181f2a687791e521d1c449c2ceca682ae6
JJimenez94 <JJimenez.9406@gmail.com>
2019-02-10 22:32:57 -0500

    Adding producer consumer program

---
+++ SortAlgorithms/Bubble.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "SortBase.h"
+
+class Bubble : public SortBase
+{
+public:
+  Bubble(std::string &output);
+  ~Bubble() = default;
+  void SortLine(std::string &currentLine) override;
+};
